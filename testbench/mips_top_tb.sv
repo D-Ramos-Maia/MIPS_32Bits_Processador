ad60ac658b6a8e0ea079df0b3aa42fc76cdd1b98
D-Ramos-Maia <daniel.r.r.maia@gmail.com>
2026-05-02 17:00:50 -0400

    Update mips_top_tb.sv

--- testbench/mips_top_tb.sv
+++ testbench/mips_top_tb.sv
@@ -36,8 +36,8 @@ module mips_top_tb;
         // Monitoramento no terminal
         // Adicionei os registradores s0 (16) e s1 (17) para vermos a mágica acontecer
         $monitor("Tempo: %0t | PC: %h | Instr: %h | ALU: %h | $s0: %h | $s1: %h", 
-                 $time, dut.pc_out, dut.instr, dut.alu_result, 
-                 dut.reg_file.regs[16], dut.reg_file.regs[17]);
+         $time, dut.pc_out, dut.instr, dut.alu_result, 
+         dut.reg_file.registers[16], dut.reg_file.registers[17]);
 
         // Tempo suficiente para rodar o seu programa .mem e o loop de Jump
         #250; 
