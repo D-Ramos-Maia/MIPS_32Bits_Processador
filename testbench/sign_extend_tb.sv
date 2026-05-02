1c69a2fe18adcec16742da69e35587d3999b1683
D-Ramos-Maia <daniel.r.r.maia@gmail.com>
2026-05-02 15:52:18 -0400

    Update sign_extend_tb.sv

--- testbench/sign_extend_tb.sv
+++ testbench/sign_extend_tb.sv
@@ -7,6 +7,11 @@ module sign_extend_tb;
     sign_extend dut (.a(a), .y(y));
 
     initial begin
+
+        //Esse script gera um arquivo .vcd, remova o comentario caso queira essa função
+        $dumpfile("sign_extend_tb.vcd"); // O arquivo nasce aqui
+        $dumpvars(0, sign_extend_tb);   // Aqui ele começa a gravar
+        
         // Teste valor positivo (5)
         a = 16'h0005;
         #10 $display("In: %h | Out: %h", a, y); // Deve sair 00000005
