97b3e00fefdb12e869464e24450efe7af9864350
D-Ramos-Maia <daniel.r.r.maia@gmail.com>
2026-05-02 23:13:41 -0400

    Update control_unit_tb.sv

--- testbench/control_unit_tb.sv
+++ testbench/control_unit_tb.sv
@@ -32,7 +32,7 @@ module control_unit_tb;
 
     initial begin
 
-        //Esse script gera um arquivo .vcd, remova o comentario caso queira essa função
+    //----Esse script gera um arquivo .vcd, remova o comentario caso queira essa função----
         //$dumpfile("control_unit_tb.vcd"); // O arquivo nasce aqui
         //$dumpvars(0, control_unit_tb);   // Aqui ele começa a gravar
         
